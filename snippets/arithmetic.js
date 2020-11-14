363fa38a1a21ec06f3342edd0c477d4284b4ee9c
Bermarte <bernardo.martelli@gmail.com>
2020-11-14 02:44:53 +0100

    Completing operations.html providing an exercise at the end

---
+++ snippets/arithmetic.js
@@ -0,0 +1,27 @@
+'use strict';
+
+console.log('--- arithmetic operators ---');
+
+let x = 40 + 4;
+
+console.log('40 + 4 =',x);
+
+x = 40 - 4;
+
+console.log('40 - 4 =', x);
+
+x = 40 * 4;
+
+console.log('40 * 4 =', x);
+
+x = 40 / 4;
+
+console.log('40 / 4 =', x);
+
+x = 40 % 4;
+
+console.log('40 % 4 =', x);
+
+x = 41 % 4;
+
+console.log('41 % 4 =', x);
